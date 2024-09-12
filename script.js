4e8c949344c2669c86a02bcccc26f9e9db120e54
Vastcope <developer@vastcope.com>
2024-09-12 11:43:55 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+alert('Amamr dokan e apnake shagotom')
+var taka = 100;
+if (taka >= 1000) {
+    console.log("apni 4 ti boi, 3 ti khata ebong 15 ti kolom kinte parben(1000 takay)");
+} else if (taka >= 900) {
+    console.log("apni 3 ti boi, 3 ti khata ebong 15 ti kolom kinte parben(900 takay)");
+} else if (taka >= 800) {
+    console.log("apni 2 ti boi, 3 ti khata ebong 15 ti kolom kinte parben(800 takay)");
+} else if (taka >= 700) {
+    console.log("apni 2 ti boi, 2 ti khata ebong 15 ti kolom kinte parben(700 takay)");
+} else if (taka >= 600) {
+    console.log("apni 2 ti boi, 2 ti khata ebong 10 ti kolom kinte parben(600 takay)");
+} else if (taka >= 500) {
+    console.log("apni 2 ti boi, 1 ti khata ebong 10 ti kolom kinte parben(500 takay)");
+} else if (taka >= 400) {
+    console.log("apni 1 ti boi, 1 ti khata ebong 10 ti kolom kinte parben(400 takay)");
+} else if (taka >= 300) {
+    console.log("apni 1 ti boi, 1 ti khata ebong 5 ti kolom kinte parben(300 takay)");
+} else if (taka >= 200) {
+    console.log("apni 1 ti boi, ebobng 1 ti khata kinte parben(200 takay)");
+} else if (taka >= 100) {
+    console.log("apni ekti boi kinte parben(100 takay)");
+} else {
+    console.log("dukkhito, apni kichui kinte parben na, karon apnar kache porjapto taka nei");
+}
